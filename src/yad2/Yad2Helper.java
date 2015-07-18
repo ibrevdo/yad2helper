782728e3eb497266d70b4fc2dce5f137e6cdad23
c0lt-fire <c0lt_fire@yahoo.com>
2015-07-19 01:01:45 +0300

    using watchlist file as history is supported. the watchlist file are displayed after sort by dates

--- src/yad2/Yad2Helper.java
+++ src/yad2/Yad2Helper.java
@@ -12,9 +12,13 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
-import java.util.Calendar;
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -26,76 +30,40 @@ import javax.swing.JFileChooser;
  */
 public class Yad2Helper {
 	
-	static final String URL = "http://www.yad2.co.il";
-	static final String FULL_URL = "http://www.yad2.co.il/Nadlan/rent_info.php?NadlanID=";
+	private static final String URL = "http://www.yad2.co.il";
 	
-	public Map<String, ApartmentInfo> db = new HashMap<String, ApartmentInfo>();
+	private static final String FULL_URL = "http://www.yad2.co.il/Nadlan/rent_info.php?NadlanID=";
 	
-	public static void main(String[] args) throws IOException {
-		Yad2Helper yad2help = new Yad2Helper();
-		//String uriString  = "http://csb.stanford.edu/class/public/pages/sykes_webdesign/05_simple.html";		
-		//String uriString  = "http://www.yad2.co.il/Nadlan/rent.php?AreaID=5&City=&HomeTypeID=&fromRooms=&untilRooms=&fromPrice=3500&untilPrice=3500&PriceType=1&FromFloor=&ToFloor=&EnterDate=&Info=";
+	public Map<String, ApartmentInfo> mainDb = new HashMap<String, ApartmentInfo>();
+	
+	
+/*	private static Map<String,ApartmentInfo> sortByValue(Map<String,ApartmentInfo> map)
+	{
+		LinkedList<Entry<String, ApartmentInfo>> list = new LinkedList<>(map.entrySet());
 		
-		//File htmlFile = new File("yad2.example.html");
-				
-		File dbFile = new File("yad2.helper.db");
-		if(!dbFile.exists()) {
-			dbFile.createNewFile();
-		}
+		Collections.sort(list, new Comparator<Entry<String, ApartmentInfo>>() {
+
+			@Override
+			public int compare(Entry<String, ApartmentInfo> o1, Entry<String, ApartmentInfo> o2) {
+				o1.getValue().publishDate
+				return 0;
+			}
 			
+		});
 		
-		
-		//pd.downloadPage(uriString, htmlFile);
-		
-		yad2help.loadDb(dbFile);
-		
-		JFileChooser dirChooser = new JFileChooser();
-		//fd.setDirectory(System.getProperty("user.dir"));
-		dirChooser.setCurrentDirectory(new java.io.File(System.getProperty("user.dir")));
-		dirChooser.setDialogTitle("Choose folder containg HTML files");
-		dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-		dirChooser.showOpenDialog(null);
-		File folder = dirChooser.getSelectedFile();	
-		
-		//File folder = new File("./html");
-		
-		Set<String> links = new HashSet<String>();
-		
-		for (final File htmlFile : folder.listFiles()){
-			links.addAll(yad2help.getFreshLinks(htmlFile, URL));
-		}
-		
-		
-		
-		//output file
-		File outFile = new File("yad2.new.links");
-		if(!outFile.exists()) {
-			outFile.createNewFile();
-		}
-		PrintWriter out = new PrintWriter(outFile);
-		for (String link : links)
-			if (!yad2help.db.containsKey(link))
-				out.println(FULL_URL + link);	
-		out.close();
-		
-		yad2help.addToDb(links);
-		yad2help.saveDbToFile(dbFile);		
 	}
-	
+*/
 
 
-	private void loadDb(File dbFile) throws IOException {
+	private void loadDbFromFile(File dbFile) throws IOException, ParseException {
 		BufferedReader br = new BufferedReader(new FileReader(dbFile));
 		String line;
 		while ((line = br.readLine()) != null)
 		{
-			StringTokenizer tk = new StringTokenizer(line);
-			String key = tk.nextToken();
-			ApartmentInfo info = new ApartmentInfo();
-			info.publishDate = tk.nextToken();
-			info.recentDate = tk.nextToken();
-			//info.notes = tk.nextToken();
-			db.put(key, info) ;
+			String key = line.substring(0, line.indexOf(" "));
+			ApartmentInfo info = new ApartmentInfo(line.substring(line.indexOf(" ")+1));
+			info.isNew = false;
+			mainDb.put(key, info) ;
 		}
 		br.close(); 
 	}
@@ -120,35 +88,86 @@ public class Yad2Helper {
 	    }
 	    return set;
 	}
+	
+	private void parseWatchlistFile(File watchlistFile, List<ApartmentInfo> list) throws IOException, ParseException {
+		BufferedReader br = new BufferedReader(new FileReader(watchlistFile));
+		String line;
+		while ((line = br.readLine()) != null)
+		{
+			ApartmentInfo info = new ApartmentInfo(line.substring(line.indexOf("=")+1));
+			list.add(info);
+		}	
+		br.close();
+	}
 
 	
-	private void addToDb(Set<String> links) throws IOException {	
+	private void addFreshLinksToDb(Set<String> links) throws IOException, ParseException {	
 		for (String key : links) {	
-			Calendar cal = Calendar.getInstance();
-			String date = cal.get(Calendar.YEAR) + "/"
-					+ cal.get(Calendar.MONTH) + "/" 
-					+cal.get(Calendar.DAY_OF_MONTH);
-			ApartmentInfo info = db.remove(key);
+
+			ApartmentInfo info = mainDb.remove(key);
 			if (info != null)
 			{
-				info.recentDate = date;
+				info.setUpdateDateToToday();
 			}
 			else {
 				info = new ApartmentInfo();
-				info.publishDate = date;
-				info.recentDate = date;
+				info.setId(key);
 			}
-			db.put(key,info);
+			mainDb.put(key,info);
 		}
 	}
 	
-	public void saveDbToFile(File dbFile) throws IOException
+	private void updateWatchedAdsAndNotes(List<ApartmentInfo> list) {
+		for (String key : mainDb.keySet()) 
+		{
+			ApartmentInfo info = mainDb.get(key);
+			if(!info.isNew) info.isWatched = false;
+			info.updateNote("");
+		}
+		for (ApartmentInfo apartmentInfo : list) {
+			ApartmentInfo info = mainDb.get(apartmentInfo.getId());
+			info.isWatched = true;
+			info.updateNote(apartmentInfo.getNote());
+		}
+	}
+	
+	public void saveDbToFile(File file) throws IOException
 	{
-		 PrintWriter out = new PrintWriter(dbFile);
-		 for (String key : db.keySet())
+		 PrintWriter out = new PrintWriter(file);
+		 for (String key : mainDb.keySet())
 		 {
-			 ApartmentInfo info = db.get(key);
-			 out.println(key + "  " + info.publishDate + " " + info.recentDate);
+			 out.println(key + " " + mainDb.get(key));
+		 }
+		 out.close();
+	}
+	
+	public void addNewApartmentsToWatchlist(List<ApartmentInfo> list)
+	{
+		for (String key : mainDb.keySet())
+		{
+			ApartmentInfo info = mainDb.get(key);
+			if (info.isNew || info.isWatched) 
+				list.add(info);
+			//info.isNew = false;
+		}
+	}
+	
+	public void generateWatchlistFile(List<ApartmentInfo> list, String start, File file) throws IOException
+	{
+		 	 
+		 
+		 Collections.sort(list,new Comparator<ApartmentInfo>() {
+			@Override
+			public int compare(ApartmentInfo o1, ApartmentInfo o2) {
+				return o1.compareTo(o2);
+			}
+		}.reversed());
+		 
+		 PrintWriter out = new PrintWriter(file);
+		 
+		 for (ApartmentInfo info : list)
+		 {
+			 out.println(start + info);
 		 }
 		 out.close();
 	}
@@ -170,5 +189,74 @@ public class Yad2Helper {
 		FileUtils.copyURLToFile(url, htmlFile);
 		
 		return htmlFile;
+	}		
+	
+	
+	
+	public static void main(String[] args) throws IOException, ParseException, InterruptedException {
+		Yad2Helper yad2help = new Yad2Helper();
+
+		//System.setProperty("user.dir",System.getProperty("user.dir") + "/example" );
+		
+		
+		File dbFile = new File("yad2.helper.db");
+		if(!dbFile.exists()) {
+			dbFile.createNewFile();
+		}
+		
+		//pd.downloadPage(uriString, htmlFile);
+		
+		yad2help.loadDbFromFile(dbFile);
+		
+		JFileChooser dirChooser = new JFileChooser();
+		//fd.setDirectory(System.getProperty("user.dir"));
+		dirChooser.setCurrentDirectory(new java.io.File(System.getProperty("user.dir")));
+		dirChooser.setDialogTitle("Choose folder containg HTML files");
+		dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		dirChooser.showOpenDialog(null);
+		File folder = dirChooser.getSelectedFile();	
+		
+		Set<String> links = new HashSet<String>();
+		
+		if (folder != null){
+			for (final File htmlFile : folder.listFiles()){
+				links.addAll(yad2help.getFreshLinks(htmlFile, URL));
+			}
+			yad2help.addFreshLinksToDb(links);
+		}
+		
+		Thread.sleep(100);
+		
+		dirChooser = new JFileChooser();
+		dirChooser.setCurrentDirectory(new java.io.File(System.getProperty("user.dir")));
+		dirChooser.setDialogTitle("Choose watchlist file");
+		dirChooser.showOpenDialog(null);
+		File watchlistFile = dirChooser.getSelectedFile();
+		if (watchlistFile == null) {
+			watchlistFile = new File("yad2.new.links");
+
+			if(!watchlistFile.exists()) {
+				watchlistFile.createNewFile();
+			}			
+		}
+		
+		List<ApartmentInfo> list = new LinkedList<>();
+				
+		//parse watch-list file into watch-list
+		yad2help.parseWatchlistFile(watchlistFile, list);
+								
+		//update is_watched ads in main DB according watch-list
+		yad2help.updateWatchedAdsAndNotes(list);
+		
+		yad2help.saveDbToFile(dbFile);
+				
+		//add all new ads to watch-list
+		 list = new LinkedList<>();
+		yad2help.addNewApartmentsToWatchlist(list);
+		
+		//generate watch-list file
+		yad2help.generateWatchlistFile(list, FULL_URL,watchlistFile);	
+				
 	}
+
 }
